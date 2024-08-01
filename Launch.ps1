1f50a0e1423bf2dbb1e9e64a7be9ac758ecf88c4
Adri Pino <74270582+addreeh@users.noreply.github.com>
2024-08-01 10:10:46 +0200

    Add files via upload

--- Launch.ps1
+++ Launch.ps1
@@ -1,19 +1,25 @@
 $repoUrl = "https://github.com/addreeh/InitAPH/archive/refs/heads/main.zip"
 $downloadPath = "$env:TEMP\InitAPH.zip"
-$extractPath = "$env:TEMP\InitAPH"
+$baseExtractPath = "$env:TEMP\InitAPH"
 $scriptToRun = "InitAPH.ps1"
 
+Write-Output "El script se está ejecutando desde: $PSScriptRoot"
+
 if (Test-Path -Path $downloadPath) {
+    Write-Host "El archivo ZIP ya existe, eliminando..."
     Remove-Item -Path $downloadPath -Force
 }
 
-if (Test-Path -Path $extractPath) {
-    Remove-Item -Path $extractPath -Recurse -Force
+$extractPath = $baseExtractPath
+$index = 1
+
+while (Test-Path -Path $extractPath) {
+    $extractPath = "${baseExtractPath}_$index"
+    $index++
 }
 
-if (-Not (Test-Path -Path $extractPath)) {
-    New-Item -ItemType Directory -Path $extractPath
-}
+Write-Output "Creando el directorio: $extractPath..."
+New-Item -ItemType Directory -Path $extractPath -Force
 
 Write-Output "Descargando el repositorio desde $repoUrl..."
 Invoke-WebRequest -Uri $repoUrl -OutFile $downloadPath
