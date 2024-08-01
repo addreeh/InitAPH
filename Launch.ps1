2f37f247f200e0a4201c130de378271fc47fe1a7
Adri Pino <74270582+addreeh@users.noreply.github.com>
2024-08-01 09:27:25 +0200

    Add files via upload

---
+++ Launch.ps1
@@ -0,0 +1,29 @@
+$repoUrl = "https://github.com/addreeh/InitAPH/archive/refs/heads/main.zip"
+$downloadPath = "$env:TEMP\InitAPH.zip"
+$extractPath = "$env:TEMP\InitAPH"
+$scriptToRun = "InitAPH.ps1"
+
+if (-Not (Test-Path -Path $extractPath)) {
+    New-Item -ItemType Directory -Path $extractPath
+}
+
+Write-Output "Descargando el repositorio desde $repoUrl..."
+Invoke-WebRequest -Uri $repoUrl -OutFile $downloadPath
+
+Write-Output "Descomprimiendo el archivo en $extractPath..."
+Expand-Archive -Path $downloadPath -DestinationPath $extractPath
+
+$repoFolder = Get-ChildItem -Path $extractPath | Where-Object { $_.PSIsContainer } | Select-Object -First 1
+$scriptPath = Join-Path -Path $repoFolder.FullName -ChildPath $scriptToRun
+
+if (Test-Path -Path $scriptPath) {
+    Write-Output "Ejecutando el script $scriptPath..."
+    & $scriptPath
+} else {
+    Write-Output "El script $scriptToRun no se encontró en el repositorio descomprimido."
+}
+
+Remove-Item -Path $downloadPath -Force
+Remove-Item -Path $extractPath -Recurse -Force
+
+Write-Output "Proceso completado."
